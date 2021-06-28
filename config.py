4ba36fd3f27d42730dce63c3a658b3522c9898a2
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-06-29 06:59:58 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,6 @@ import os
 
 TOKEN = os.environ.get("TOKEN", "")
 ADMIN = os.environ.get("ADMIN", "")
+OWNER = os.environ.get("OWNER", "")
+GROUP = os.environ.get("GROUP", "")
+CHANNEL = os.environ.get("CHANNEL", "")
