b53a67d92be85db28bfccacfd48143a354d72a22
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-06-29 19:41:52 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -11,11 +11,10 @@ import json
 from glob import glob
 import pytz
 from datetime import datetime
-from config import TOKEN, ADMIN, OWNER, CHANNEL, GROUP, PROJECT_NAME
+from config import TOKEN, ADMIN, OWNER, CHANNEL, GROUP
 from TikTokApi import TikTokApi
 
 token = TOKEN
-
 bot = amanobot.Bot(token)
 
 queue = {
@@ -31,7 +30,7 @@ def saveConfig(data):
 
 if __name__ == '__main__':
 	s = time.time()
-	print(f'[#] Buatan\n[i] Created by @{OWNER}\n')
+	print(f'[#] Buatan\n[i] Created by {OWNER}\n')
 	print('[#] mengecek config...')
 	if not os.path.isfile('config.json'):
 		print('[#] memebuat config file...')
@@ -69,14 +68,8 @@ def handle(update):
 					bot.sendMessage(queue["occupied"][uid], "" + text)
 			
 			if 'photo' in update:
-				#papan = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="PESAN OTOMATIS", url="https://google.com")]])
 				captionphoto = update["caption"] if "caption" in update else None
-				photo = update['photo'][0]['file_id']
-				bot.sendPhoto(queue["occupied"][uid], photo, caption=captionphoto)
-				#else:
-				#	photo = update['photo'][0]['file_id']
-				#	bot.sendPhoto(queue["occupied"][uid], photo)
-
+				photo = 
 			if 'video' in update:
 				captionvideo = update["caption"] if "caption" in update else None
 				video = update['video']['file_id']
@@ -107,7 +100,6 @@ def handle(update):
 
 			if 'contact' in update:
 				nama = update["contact"]["first_name"]
-			#	nama = update["contact"]["last_name"]
 				contact = update['contact']['phone_number']
 				bot.sendContact(queue["occupied"][uid], contact, first_name=nama, last_name=None)
 			
@@ -118,61 +110,13 @@ def handle(update):
 
 		if text == "/start" or text == "/refresh":
 			if not uid in queue["occupied"]:
-				#with open('is.txt', 'r') as file:
-				#	user_ids = file.read()
-				#	if str(uid) not in user_ids:
-				#		with open('is.txt', 'w') as f:
-				#			f.write(user_ids+"\n"+str(uid))
-				#			keyboard1 = ReplyKeyboardRemove()
-				#			#keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="ᴏᴡɴᴇʀ", url=f"https://t.me/{OWNER}"),InlineKeyboardButton(text="ᴛɪᴋᴛᴏᴋ", url="https://vm.tiktok.com/ZSJC6FWw2/")]])
-				#			bot.sendMessage(uid,"_⚡️ BOT CHAT ANONYMOUS ⚡️\n\nKAMU DAPAT MENEMUKAN TEMAN\nSECARA RANDOM 🦊 TEKAN [ /start ]_", parse_mode="MarkDown", reply_markup=keyboard1, reply_to_message_id=update['message_id'])
-				#	else:
-				#keyboard1 = ReplyKeyboardMarkup(keyboard=[['Search 🔍'],['Pengguna👤','MENU BOT✅']], resize_keyboard=True, one_time_keyboard=True)
 				keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="ᴏᴡɴᴇʀ", url=f"https://t.me/{OWNER}"),InlineKeyboardButton(text="ɢʀᴜᴘ ᴄʜᴀᴛ", url=f"t.me/{GROUP}")]])
 				bot.sendMessage(uid, f"⚡️ SELAMAT DATANG DI {PROJECT_NAME} ⚡️\n\n_🇮🇩 Semoga Dapat teman atau jodoh\n🇳🇿 I hope you can make a friend or a partner\n\n?> untuk mencari teman obrolan gunakan perintah /search_", parse_mode='MarkDown', disable_web_page_preview=True , reply_markup=keyboard)
-						#bot.sendMessage(uid,"_[❗️] Follow sosial media dan support terus bot ini_",parse_mode="MarkDown", reply_markup=keyboard1)
-
 		if 'message_id' in update:
 			if not uid in queue["occupied"]:
 				if text != "/start" and text != "Pengguna👤" and text !="Next ▶️" and text != "/refresh" and text != "/help" and text != "/search" and text != "Search 🔍" and text != "MENU BOT✅" and text != "🔙 Main Menu" and text != "/trendingtiktok" and text != "RandomPhoto📷" and text != "Info Profile 📌" and text != "Covid-19〽️" and text != "/mabar" and text != "Link Kejutan" and text != "Youtube▶️" and text != "/user":
 					news = ReplyKeyboardRemove()
-					#news = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="ɢʀᴏᴜᴘ ᴄʜᴀᴛ", url=f"t.me/{GROUP}"), InlineKeyboardButton(text="𝔽𝕆𝕃𝕃𝕆𝕎 𝕄𝔼", url=f"https://t.me/{OWNER}")]])
 					bot.sendMessage(uid, "_[❗️] Maap kamu sedang tidak dalam obrolan\nSilahkan Klik /refresh atau /search pada bot_", parse_mode="MarkDown",reply_markup=news, reply_to_message_id=update['message_id'])
-					# pesan = bot.sendMessage(uid, "Wait...", reply_markup=keyboarddihapus)
-					# time.sleep(4)
-					# hapus = pesan['message_id']
-					# bot.deleteMessage((uid,hapus))	
-		#if 'text' in update and update['text'] == '/pis':
-		#	with open('id.txt', 'r') as file:
-		#		user_ids = file.read()
-		#		if str(uid) not in user_ids:
-		#			with open('id.txt', 'w') as f:
-		#				f.write(user_ids+"\n"+str(uid))
-		#			bot.sendMessage(uid,"Id saved")
-		#		else:
-		#			bot.sendMessage(uid, "kmu sudah ada di bot")
-			#with open('./id.txt', 'r') as idfile:
-			#	chat_id=int(idfile.read())
-			#	bot.sendMessage(chat_id, "Someone is in your house!")
-			
-		#if text == "/bs":
-		#	text = " ".join(update["text"].split()[1:])
-		#	# = json.loads(open("id.txt", "r").read())
-		#	try:
-		#		for uid in users:
-		#			bot.sendMessage(int(uid), text)
-		#	except:
-		#		raise
-		#if update["text"].split()[0] == "/bc":
-		#	text = update["text"].split()
-		#	if len(text) == 0:
-		#		return bot.sendMessage(uid, "masukkan text")
-		#	try:
-		#		for uid in user3:
-		#			bot.sendMessage(uid, " ".join(text[1:]))
-		#	except:
-		#		pass
-
 		if text == "/mabar":
 			if not uid in queue["occupied"]:
 				if str(uid) in ADMIN :
@@ -220,13 +164,8 @@ def handle(update):
 				bot.sendMessage(uid, "⚡️ Perintah ini hanya untuk admin ⚡️")
 		elif text == 'Info Profile 📌':
 			if str(uid) in ADMIN :
-				#if "last_name" not in update["from"]:
-				#	return bot.sendMessage(uid, "Harap Isi Nama Belakang Kamu!!")
-				#if not update["from"]["last_name"] == None else update["from"]["last_name"]:
-				#lastname = update["from"]["last_name"] if "last_name" in update else None
 				name = update["from"]["first_name"]
 				_id = update["from"]["id"]
-				#username = update["from"]["username"] if "username" not in update None
 				username = update["from"]["username"]
 				tipe = update["chat"]["type"]
 				date1 = datetime.fromtimestamp(update["date"], tz=pytz.timezone("asia/jakarta")).strftime("%d/%m/%Y %H:%M:%S").split()
@@ -238,7 +177,6 @@ def handle(update):
 				text += "*Waktu :* " + str(date1[1]) + " WIB" "\n"
 				bot.sendMessage(uid, text, parse_mode='MarkDown', reply_to_message_id=update['message_id'])
 			else:
-				#forw = update["forward_from"]['language_code']
 				bahasa = update["from"]["language_code"]
 				name = update["from"]["first_name"]
 				_id = update["from"]["id"]
@@ -263,7 +201,7 @@ def handle(update):
 			keyboard = ReplyKeyboardMarkup(keyboard=[
 				['Info Profile 📌','Covid-19〽️'],['🔙 Main Menu']
 			], resize_keyboard=True, one_time_keyboard=True)
-			bot.sendMessage(uid, f"Welcome My boo🙊\nYuk Join My Grup @{GROUP} dan Channel @{CHANNEL} :)", reply_markup=keyboard)
+			bot.sendMessage(uid, "Welcome My boo🙊\nYuk Join My Grup (f"@{GROUP}") dan Channel (f"@{CHANNEL}") :)", reply_markup=keyboard)
 
 		elif text == 'Covid-19〽️':
 			web = requests.get('https://www.worldometers.info/coronavirus/country/indonesia/')
@@ -272,31 +210,9 @@ def handle(update):
 			ouy = "*KASUS VIRUS COVID-19 DI INDONESIA 🇮🇩*\n\nTerpapar Virus : {} Orang\nMeninggal : {} Orang\nSembuh : {} Orang".format(dataweb[0].span.text,dataweb[1].span.text,dataweb[2].span.text)
 			bot.sendMessage(uid, ouy, parse_mode='MarkDown')
 			
-		#elif text == "Youtube▶️" or text == "/subscribe":
-		#	bot.sendMessage(uid, 'Yuk Nonton Video Video aku, Cek di button keyboard', reply_markup=InlineKeyboardMarkup(inline_keyboard=[
-		#		[InlineKeyboardButton(text="SUBSCRIBE YOUTUBE", url='https://youtube.com/channel/UCE6TQ4yG8eNEiOzqRSfOu-w')],
-		#		[InlineKeyboardButton(text="FILM PENDEK", url="https://youtu.be/35wJMkvSlQU" )],
-		#		[InlineKeyboardButton(text="BOT JADWAL SHOLAT", url='https://youtu.be/YRcKu-kZd0o'), InlineKeyboardButton(text='NEW FITUR BOT', url='https://youtu.be/TKmSmDBLuos'), InlineKeyboardButton(text='DOWNLOAD VIDEO TIKTOK', url='https://youtu.be/P0iBcUAGuzE')],
-		#		[InlineKeyboardButton(text=">>",url="https://youtu.be/YRcKu-kZd0o")]
-		#	]))
-
-		#elif text == 'Link Kejutan':
-		#	bot.sendMessage(uid, 'Silahkan Klik Kejutan Dari aku by😙',reply_markup = InlineKeyboardMarkup(inline_keyboard=[
-         #                           [InlineKeyboardButton(text="Kejutan 1😍", url='https://realsht.mobi/V3Mpf'), InlineKeyboardButton(text="Kejutan 2😍",url='https://realsht.mobi/A3Zmz')],
-        #                            [InlineKeyboardButton(text="Kejutan 3😍", url='https://realsht.mobi/o2XuR')]
-         #                       ]
-         #                   ))	
-
 		elif text == '🔙 Main Menu':
 			keyboard = ReplyKeyboardMarkup(keyboard=[['Search 🔍'],['Pengguna👤','MENU BOT✅']], resize_keyboard=True, one_time_keyboard=True)
 			bot.sendMessage(uid, "_🔄 Kembali_", parse_mode='MarkDown', disable_web_page_preview=True, reply_markup=keyboard)
-
-		#elif text == 'RandomPhoto📷':
-		#	picls = glob("img/*.jpg")
-		#	love = random.choice(picls)
-		#	with open(love, 'rb') as photo:
-		#		bot.sendPhoto(uid, photo)
-
 		elif text == "Next ▶️" or text == "/next" and uid in queue["occupied"]:
 			print('[SB] ' + str(uid) + ' meninggalkan obrolan dengan ' + str(queue["occupied"][uid]))
 			keyboard = ReplyKeyboardMarkup(keyboard=[['Search 🔍', '🔙 Main Menu']], resize_keyboard=True, one_time_keyboard=True)
@@ -322,7 +238,7 @@ def handle(update):
 			partner = random.choice(exList(queue["free"], uid))
 			if partner != uid:
 				keyboard = ReplyKeyboardMarkup(keyboard=[
-					["🎲","🎯","🎳","🏀","⚽","🎰"],['Next ▶️', '❌ Exit'],[dict(text='Send my phone', request_contact=True)]
+					['Next ▶️', '❌ Exit'],[dict(text='Send my phone', request_contact=True)]
 				],resize_keyboard=True, one_time_keyboard=True)
 				print('[SB] ' + str(uid) + ' Berjodoh dengan ' + str(partner))
 				queue["free"].remove(partner)
