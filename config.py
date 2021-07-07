bde30b74f7d5e83c9a50341e88e467120eb951b0
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-07-07 20:55:06 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", 12345))
-OWNER = os.environ.get("OWNER", "")
+TOKEN = os.environ.get("TOKEN", "1782413801:AAFzU9UlkWiV5SF-1-OagPhfwYVO5CXnj9w")
+ADMIN = int(os.environ.get("ADMIN", "1641726479"))
+OWNER = os.environ.get("OWNER", "kenkanasw")
 GROUP = os.environ.get("GROUP", "musikkugroup")
 CHANNEL = os.environ.get("CHANNEL", "musikkuchannel")
 PROJECT_NAME = os.environ.get("PROJECT_NAME", "PEIVATE CHAT")
